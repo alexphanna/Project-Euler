705aa33dce48252f95d47f5a2725c7c1ca23d48c
Alex Hanna <ahanna0608@gmail.com>
2023-11-30 15:43:34 -0500

    Add problem 6

---
+++ 6.py
@@ -0,0 +1,14 @@
+def main():
+    square_of_sum = 0
+    for i in range(1, 101):
+        square_of_sum += i
+    square_of_sum = square_of_sum**2
+
+    sum_of_squares = 0
+    for i in range(1, 101):
+        sum_of_squares += i**2
+
+    print(square_of_sum - sum_of_squares)
+
+if __name__ == '__main__':
+    main()
