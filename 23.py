87d6988d18f2542797b79dff83dcf56c9443f154
Alex Hanna <ahanna0608@gmail.com>
2023-12-08 19:26:08 -0500

    Add problem 23

--- 23.py
+++ 23.py
@@ -1,39 +1,35 @@
 import math
 import multiprocessing
 
+divisors = []
 def abundant_number(number):
-    if not number % 2 == 0 and not number % 3 == 0:
-        return
-    if number < 945 and number % 2 == 1:
-        return
-    sum = 0
-    for i in range(1, math.ceil(math.sqrt(number))):
-        if number % i == 0:
-            sum += number
-            if i != number / i and i != 1:
-                sum += number / i
-
-    return sum > number
+    return divisors[number - 1] > number
 
 def main():
+    for i in range(1, 28123 + 1):
+        sum = 0
+        for j in range(1, int(math.sqrt(i)) + 1):
+            if i % j == 0:
+                sum += j
+                if i / j != j and j != 1:
+                    sum += i / j
+        divisors.append(sum)
+
     pool = multiprocessing.Pool(12)
 
-    non_abundant_sums = pool.map(abundant_sum, range(1, 28123))
+    non_abundant_sums = pool.map(abundant_sum, range(1, 28123 + 1))
 
     total = 0
     for non_abundant_sum in non_abundant_sums:
-        if non_abundant_sum is int:
+        if not non_abundant_sum is None:
             total += non_abundant_sum
     print(total)
 
 def abundant_sum(number):
-    print(number)
-    abundant = False
     for i in range(1, number):
         if(abundant_number(i) and abundant_number(number - i)):
-            abundant = True
-    if not abundant:
-        return number
+            return None
+    return number
 
                 
 
